59fe2d36383bafe7992846d799f913881cdec97d
Satya <satyaganesh399@gmail.com>
2022-09-23 21:56:17 +0530

    functionality added and weather shows accordingly

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import WeatherApp from './Components/WeatherApp'
 function App() {
+  
   return (
     <div className="App">
       <WeatherApp />
--- src/Components/WeatherApp.js
+++ src/Components/WeatherApp.js
@@ -1,45 +1,59 @@
 import React from "react";
-import axios from "axios";
+
 export default function WeatherApp() {
-  let [weatherData, setWeatherData] = React.useState({});
-  const [location, setLocation] = React.useState("New York");
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=visakhapatnam&units=metric&appid=307d05c02ae2261eb06920a7bf055b1a`;
+  let [weatherData, setWeatherData] = React.useState('');
+  const [location, setLocation] = React.useState("");
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=307d05c02ae2261eb06920a7bf055b1a`;
+
+  function saveLocation(e) {
+    setLocation(e.target.value);
+  }
+
   let fetchData = function (event) {
     if (event.key === "Enter") {
-      axios.get(url).then((response) => {
-        setWeatherData(response.data);
-        console.log(weatherData);
-      });
+      fetch(url)
+        .then((res) => res.json())
+        .then((res) => setWeatherData(res))
     }
   };
 
   return (
-   
-      <div className="weather-app">
-        <input type="text"
-                name="search-box"
-                onChange={event => fetchData(event.target.value)}
-                onKeyPress = {fetchData}
-                placeholder="Enter location"/>
-        <div className="top-side">
-          <h4>Lucknow</h4>
-          <h2>36 C</h2>
-          <h4 className="rotate-element">Partly Coud</h4>
+    <div className="weather-app">
+      <input
+        type="text"
+        name="search-box"
+        onChange={saveLocation}
+        onKeyPress={fetchData}
+        placeholder="Enter location"
+        value = {location}
+      />
+
+      {weatherData.name ? (
+        <div>
+         <div className="top-side">
+            <h4>{weatherData.name}</h4>
+            <h2>{weatherData.main.temp.toFixed()}°C</h2>
+            <h4 className="rotate-element">{weatherData.weather[0].main}</h4>
+          </div>
+          <div className="bottom-side">
+            <div className="feels-like">
+              <h4>{weatherData.main.feels_like.toFixed()}°C</h4>
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              <h4>{weatherData.main.humidity}%</h4>
+              <p>Humidity</p>
+            </div>
+            <div className="wind-speed">
+              <h4>{weatherData.wind.speed.toFixed()} Kmph</h4>
+              <p>Wind Speed</p>
+            </div>
+          </div> 
         </div>
-        <div className="bottom-side">
-          <div className="feels-like">
-            <h4>38 C</h4>
-            <p>Feels Like</p>
-          </div>
-          <div className="humidity">
-            <h4>20%</h4>
-            <p>Humidity</p>
-          </div>
-          <div className="wind-speed">
-            <h4>12 Kmph</h4>
-            <p>Wind Speed</p>
-          </div>
-        </div>
-      </div>
+      ) : <div className="error-message">Note : Enter valid location</div>}
+    </div>
   );
 }
+
+
+ 
